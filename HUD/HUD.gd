e73089309787c9905b6280d1054ad15e15dfd849
Teyn <valentina.fey.al@gmail.com>
2024-06-03 09:35:26 -0600

    x

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -1,23 +1,32 @@
 extends CanvasLayer
 
+signal start_game_btn
+
 var score = 0
-var highscore= 0
+var highscore = 0
 var is_playing = false 
 
 func update_score_label():
 	score = score + 10
 	$ScoreLabel.text = "Score: %d"%(score)
+	
 
 func update_high_score_label():
 	if score > highscore:
 		highscore = score
 		$HighScorelabel.text = "Highscore: %d"%(highscore)
+	is_playing = false
+	$ScoreTimer.stop()
+	$Button.visible = true
 
 func _on_button_pressed():
+	score = 0
+	update_score_label()
+	start_game_btn.emit()
 	is_playing = true
 	$ScoreTimer.start()
 	$Button.visible = false
-	
+
 
 
 func _on_score_timer_timeout():
